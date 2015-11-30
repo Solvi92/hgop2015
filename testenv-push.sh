474a037beb06918ffc05239662e2594b7a8a4d05
Solvi92 <solvihj@gmail.com>
2015-11-30 15:49:24 +0000

    finished day 2, starting on jenkins

--- testenv-push.sh
+++ testenv-push.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
 docker push solvih13/tictactoe
+ssh -i ~/.ssh/id_rsa2 vagrant@127.0.0.1 -p 2222 "\
+	docker pull solvih13/tictactoe; \
+	docker stop ttt; \
+	docker rm ttt; \
+	docker run -p 9090:8080 -d --name ttt -e \"NODE_ENV=production\" solvih13/tictactoe;"
